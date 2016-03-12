c2718bb3a4b13ed6cf875453b6f2073cb73b3a42
sbrkl <ruankangle123@126.com>
2016-03-12 16:29:58 +0800

    server start

--- server.js
+++ server.js
@@ -14,3 +14,39 @@ function sendFile(response, filePath, fileContents) {
   response.writeHead(200,{"content-type": mime.lookup(path.basename(filePath))});
   response.end(fileContents);
 }
+
+function serveStatic(response, cache, absPath) {
+  if(cache[absPath]) {
+    sendFile(response, absPath, cache[absPath]);
+  } else {
+    fs.exists(absPath, function(exists) {
+      if(exists){
+        fs.readFile(absPath, function(err, data){
+          if(err){
+            send404(response)
+          } else{
+            cache[absPath] = data;
+            sendFile(response,absPath,data);
+          }
+        })
+      } else {
+        send404(response);
+      }
+    })
+  }
+}
+
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+  if (request.url == '/') {
+    filePath = 'public/index.html';
+  } else {
+    filePath = 'public' + request.url;
+  }
+  var absPath = './' + filePath;
+  serveStatic(response, cache, absPath);
+});
+
+server.listen(3000, function() {
+  console.log("Server listening on port 3000.");
+});
